e5ef8caaca98c8ffa46367471ed19379d14f360e
ddoniddoni <psdkei@naver.com>
2020-09-16 14:32:41 +0900

    Update Sidebar Icon

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import { Avatar, IconButton } from "@material-ui/core";
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h1>SideBar</h1>
+      <div className="sidebar__header">
+        <div className="sidebar__headeRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <ChatIcon />
+        </div>
+      </div>
     </div>
   );
 };
