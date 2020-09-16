8a647575e2420b7b85eedc643dbbada4c511d9e8
ddoniddoni <psdkei@naver.com>
2020-09-16 13:06:37 +0900

    Setting All Size

---
+++ whatsapp-clone/src/Chat.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Chat.css";
+const Chat = () => {
+  return (
+    <div>
+      <h1>i'm Chat</h1>
+    </div>
+  );
+};
+
+export default Chat;
---
+++ whatsapp-clone/src/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Sidebar.css";
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h1>SideBar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
