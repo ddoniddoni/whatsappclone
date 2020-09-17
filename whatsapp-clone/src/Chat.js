cb73c70283fd05b1002c39b6fb00db0cf8da762e
ddoniddoni <psdkei@naver.com>
2020-09-17 18:32:08 +0900

    Update Chat

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -10,10 +10,12 @@ const Chat = () => {
     <div className="chat">
       <div className="chat__header">
         <Avatar />
+
         <div className="chat__headerInfo">
           <h3>Room name</h3>
           <p>Last seen at...</p>
         </div>
+
         <div className="chat__headerRight">
           <IconButton>
             <SearchOutlined />
@@ -25,6 +27,20 @@ const Chat = () => {
             <MoreVert />
           </IconButton>
         </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Sonny</span>
+          This is a Message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Sonny</span>
+          This is a Message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
       </div>
     </div>
   );
