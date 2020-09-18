d8d18ae414c39ed8fe6cbf9bf4b952cbeb2c11a0
ddoniddoni <psdkei@naver.com>
2020-09-18 20:31:46 +0900

    Link Pusher

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import Pusher from "pusher-js";
 
 const App = () => {
+  useEffect(() => {
+    const pusher = new Pusher("d1775659e7fbf40cca23", {
+      cluster: "ap3",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
